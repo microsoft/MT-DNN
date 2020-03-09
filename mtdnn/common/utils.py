14fa853afa2c7521a18c685faf20b63b6bdb4a3c
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-03-09 16:23:50 +0000

    MT-DNN Feature release v1.0.0
    ignore: IDE meta files
    add: initial checkin
    feat: make package pip installable
    change the egg
    doc: add contribution steps and update readme with additional information
    fix: wrong file references
    add: package dependencies like PyTorch transformer support
    fix import statements
    feat: add download utils
    feat: add download shell script
    doc: update readme with testing instructions
    doc: add data downloading and processing step
    feat: move fit and predict into MTDNN Model
    feat: make logger create a new log file each run
    remove: fit and predict functions from the pipeline class
    update: remove stale references
    doc: fit and predict now on the model object
    formatting code snippet
    doc: add pip install steps

---
+++ mtdnn/common/utils.py
@@ -0,0 +1,146 @@
+# coding=utf-8
+# Copyright (c) Microsoft. All rights reserved.
+# Some code referenced from https://github.com/microsoft/nlp-recipes
+import json
+import logging
+import math
+import os
+import subprocess
+import tarfile
+import zipfile
+from contextlib import contextmanager
+from logging import Logger
+from tempfile import TemporaryDirectory
+
+import requests
+import torch
+from tqdm import tqdm
+
+
+class MTDNNCommonUtils:
+    @staticmethod
+    def set_environment(seed, set_cuda=False):
+        random.seed(seed)
+        numpy.random.seed(seed)
+        torch.manual_seed(seed)
+        if torch.cuda.is_available() and set_cuda:
+            torch.cuda.manual_seed_all(seed)
+
+    @staticmethod
+    def patch_var(v, cuda=True):
+        if cuda:
+            v = v.cuda(non_blocking=True)
+        return v
+
+    @staticmethod
+    def get_gpu_memory_map():
+        result = subprocess.check_output(
+            ["nvidia-smi", "--query-gpu=memory.used", "--format=csv,nounits,noheader"],
+            encoding="utf-8",
+        )
+        gpu_memory = [int(x) for x in result.strip().split("\n")]
+        gpu_memory_map = dict(zip(range(len(gpu_memory)), gpu_memory))
+        return gpu_memory_map
+
+    @staticmethod
+    def get_pip_env():
+        result = subprocess.call(["pip", "freeze"])
+        return result
+
+    @staticmethod
+    def load_pytorch_model(local_model_path: str = ""):
+        state_dict = None
+        assert os.path.exists(local_model_path), "Model File path doesn't exist"
+        state_dict = torch.load(local_model_path)
+        return state_dict
+
+    @staticmethod
+    def dump(path, data):
+        with open(path, "w") as f:
+            json.dump(data, f)
+
+    @staticmethod
+    def generate_decoder_opt(enable_san, max_opt):
+        opt_v = 0
+        if enable_san and max_opt < 3:
+            opt_v = max_opt
+        return opt_v
+
+    @staticmethod
+    def setup_logging(filename="run.log", mode="w") -> Logger:
+        logger = logging.getLogger(__name__)
+        log_file_handler = logging.FileHandler(
+            filename="run.log", mode=mode, encoding="utf-8"
+        )
+        log_formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        log_file_handler.setFormatter(log_formatter)
+        do_add_handler = True
+        for handler in logger.handlers:
+            if isinstance(handler, logging.FileHandler):
+                do_add_handler = False
+        if do_add_handler:
+            logger.addHandler(log_file_handler)
+        logger.setLevel(logging.DEBUG)
+        return logger
+
+    @staticmethod
+    def create_directory_if_not_exists(dir_path: str):
+        os.makedirs(dir_path, exist_ok=True)
+
+    @staticmethod
+    @contextmanager
+    def download_path(path=None):
+        tmp_dir = TemporaryDirectory()
+        if not path:
+            path = tmp_dir.name
+        else:
+            path = os.path.realpath(path)
+
+        try:
+            yield path
+        finally:
+            tmp_dir.cleanup()
+
+    @staticmethod
+    def maybe_download(url, filename=None, work_directory=".", expected_bytes=None):
+        """Download a file if it is not already downloaded.
+
+        Args:
+            filename (str): File name.
+            work_directory (str): Working directory.
+            url (str): URL of the file to download.
+            expected_bytes (int): Expected file size in bytes.
+        Returns:
+            str: File path of the file downloaded.
+        """
+        if filename is None:
+            filename = url.split("/")[-1]
+        os.makedirs(work_directory, exist_ok=True)
+        filepath = os.path.join(work_directory, filename)
+        if not os.path.exists(filepath):
+            if not os.path.isdir(work_directory):
+                os.makedirs(work_directory)
+            r = requests.get(url, stream=True)
+            total_size = int(r.headers.get("content-length", 0))
+            block_size = 1024
+            num_iterables = math.ceil(total_size / block_size)
+
+            with open(filepath, "wb") as file:
+                for data in tqdm(
+                    r.iter_content(block_size),
+                    total=num_iterables,
+                    unit="KB",
+                    unit_scale=True,
+                ):
+                    file.write(data)
+        else:
+            log.debug("File {} already downloaded".format(filepath))
+        if expected_bytes is not None:
+            statinfo = os.stat(filepath)
+            if statinfo.st_size != expected_bytes:
+                os.remove(filepath)
+                raise IOError("Failed to verify {}".format(filepath))
+
+        return filepath
---
+++ scripts/download.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+############################################################## 
+# This script is used to download resources for MT-DNN experiments
+############################################################## 
+
+BERT_DIR=$(pwd)/../mt_dnn_models
+if [ ! -d ${BERT_DIR}  ]; then
+  echo "Create a folder BERT_DIR"
+  mkdir ${BERT_DIR}
+fi
+
+## Download bert models
+wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_model_base_v2.pt -O "${BERT_DIR}/bert_model_base_uncased.pt"
+wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_model_large_v2.pt -O "${BERT_DIR}/bert_model_large_uncased.pt"
+wget https://mrc.blob.core.windows.net/mt-dnn-model/bert_base_chinese.pt -O "${BERT_DIR}/bert_model_base_chinese.pt"
+
+## Download MT-DNN models
+wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_base.pt -O "${BERT_DIR}/mt_dnn_base_uncased.pt"
+wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_large.pt -O "${BERT_DIR}/mt_dnn_large_uncased.pt"
+
+## MT-DNN-KD
+wget https://mrc.blob.core.windows.net/mt-dnn-model/mt_dnn_kd_large_cased.pt -O "${BERT_DIR}/mt_dnn_kd_large_cased.pt"
+
+## Download XLNet model
+wget https://storage.googleapis.com/xlnet/released_models/cased_L-24_H-1024_A-16.zip -O "xlnet_cased_large.zip"
+unzip xlnet_cased_large.zip 
+mv xlnet_cased_L-24_H-1024_A-16/spiece.model "${BERT_DIR}/xlnet_large_cased_spiece.model"
+rm -rf *.zip xlnet_cased_L-24_H-1024_A-16
+## download converted xlnet pytorch model
+wget https://mrc.blob.core.windows.net/mt-dnn-model/xlnet_model_large_cased.pt -O "${BERT_DIR}/xlnet_model_large_cased.pt"
+
+
+## download ROBERTA
+wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.base.tar.gz -O "roberta.base.tar.gz"
+wget https://dl.fbaipublicfiles.com/fairseq/models/roberta.large.tar.gz -O "roberta.large.tar.gz"
+tar xvf roberta.base.tar.gz
+mv "roberta.base" "${BERT_DIR}/"
+tar xvf roberta.large.tar.gz
+mv "roberta.large" "${BERT_DIR}/"
+rm "roberta.base.tar.gz"
+rm "roberta.large.tar.gz"
+
+mkdir "${BERT_DIR}/roberta"
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/encoder.json' -O "${BERT_DIR}/roberta/encoder.json"
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/vocab.bpe' -O "${BERT_DIR}/roberta/vocab.bpe"
+wget -N 'https://dl.fbaipublicfiles.com/fairseq/gpt2_bpe/dict.txt' -O "${BERT_DIR}/roberta/ict.txt"
+
+if [ "$1" == "model_only" ]; then
+  exit 1
+fi
+
+DATA_DIR=$(pwd)/data
+if [ ! -d ${DATA_DIR}  ]; then
+  echo "Create a folder $DATA_DIR"
+  mkdir ${DATA_DIR}
+fi
+
+## DOWNLOAD GLUE DATA
+## Please refer glue-baseline install requirments or other issues.
+git clone https://github.com/jsalt18-sentence-repl/jiant.git
+cd jiant
+python scripts/download_glue_data.py --data_dir $DATA_DIR --tasks all
+
+cd ..
+rm -rf jiant
+#########################
+
+## DOWNLOAD SciTail
+cd $DATA_DIR
+wget http://data.allenai.org.s3.amazonaws.com/downloads/SciTailV1.1.zip
+unzip SciTailV1.1.zip
+mv SciTailV1.1 SciTail
+# remove zip files
+rm *.zip
+
+## Download preprocessed SciTail/SNLI data for domain adaptation
+cd $DATA_DIR
+DOMAIN_ADP="domain_adaptation"
+echo "Create a folder $DATA_DIR"
+mkdir ${DOMAIN_ADP}
+
+wget https://mrc.blob.core.windows.net/mt-dnn-model/data.zip 
+unzip data.zip
+mv data/* ${DOMAIN_ADP}
+rm -rf data.zip
+rm -rf data
+
+## Download SQuAD & SQuAD v2.0 data
+cd $DATA_DIR
+mkdir "squad"
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $DATA_DIR/squad/train.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $DATA_DIR/squad/dev.json
+
+mkdir "squad_v2"
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json -O $DATA_DIR/squad_v2/train.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O $DATA_DIR/squad_v2/dev.json
+
+# NER
+cd $DATA_DIR
+mkdir "ner"
+wget https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.train -O "ner/train.txt"
+wget https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.testa -O "ner/valid.txt"
+wget https://raw.githubusercontent.com/synalp/NER/master/corpus/CoNLL-2003/eng.testb -O "ner/test.txt"
