35d8d02823b48c6ba3667f59107452e4ec7cec0a
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-05-12 15:52:41 +0000

    update types for MRC, MaskLM and EncoderModels

--- mtdnn/common/types.py
+++ mtdnn/common/types.py
@@ -9,15 +9,20 @@ class TaskType(IntEnum):
     Ranking = 3
     Span = 4
     SequenceLabeling = 5
+    MaskLM = 6
 
 
 class DataFormat(IntEnum):
     PremiseOnly = 1
     PremiseAndOneHypothesis = 2
     PremiseAndMultiHypothesis = 3
-    Sequence = 4
+    MRC = 4
+    Sequence = 5
+    MLM = 6
 
 
 class EncoderModelType(IntEnum):
     BERT = 1
     ROBERTA = 2
+    XLNET = 3
+    SAN = 4
