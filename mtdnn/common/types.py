f78f5e938e8a04cf66e07071917e025045d12191
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-05-28 14:18:45 +0000

    add: Task definition type to drive processes

--- mtdnn/common/types.py
+++ mtdnn/common/types.py
@@ -27,3 +27,43 @@ class EncoderModelType(IntEnum):
     ROBERTA = 2
     XLNET = 3
     SAN = 4
+
+
+class TaskDefType(dict):
+    def __init__(
+        self,
+        label_vocab,
+        n_class,
+        data_format,
+        task_type,
+        metric_meta,
+        split_names,
+        enable_san,
+        dropout_p,
+        loss,
+        kd_loss,
+        data_paths,
+    ):
+        """
+            :param label_vocab: map string label to numbers.
+                only valid for Classification task or ranking task.
+                For ranking task, better label should have large number
+        """
+        super().__init__(
+            **{k: repr(v) for k, v in locals().items()}
+        )  # ensure the class is JSON serializable
+        self.label_vocab = label_vocab
+        self.n_class = n_class
+        self.data_format = data_format
+        self.task_type = task_type
+        self.metric_meta = metric_meta
+        self.split_names = split_names
+        self.enable_san = enable_san
+        self.dropout_p = dropout_p
+        self.loss = loss
+        self.kd_loss = kd_loss
+        self.data_paths = data_paths
+
+    @classmethod
+    def from_dict(cls, dict_rep):
+        return cls(**dict_rep)
