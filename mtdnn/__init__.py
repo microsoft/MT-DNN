e5c3e07f3a8e55067433714ce261a6d28ba73d22
Emmanuel Awa <emmanuel.awa@microsoft.com>
2020-07-01 22:22:06 -0400

    Update __init__.py

--- mtdnn/__init__.py
+++ mtdnn/__init__.py
@@ -5,7 +5,7 @@ __title__ = "Microsoft MT-DNN"
 __author__ = "Microsoft Research AI"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-present Microsoft Corporation"
-__version__ = "0.0.0"
+__version__ = "1.1.0"
 
 # Synonyms
 TITLE = __title__
